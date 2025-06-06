210782dcadc8f75e2534ecf1dd81bb0964c4e021
Roger <rogervtsc@hotmail.com>
2025-06-06 20:07:21 -0300

    backend: chore prisma config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './services/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- backend/src/services/prisma.service.ts
+++ backend/src/services/prisma.service.ts
@@ -1,9 +1,27 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super();
+  }
+
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+      console.log('Conexão com o banco de dados estabelecida com sucesso.');
+    } catch (error) {
+      console.error('Erro ao conectar ao banco de dados:', error);
+      // Dependendo da sua estratégia de tratamento de erros, você pode relançar o erro ou sair do aplicativo
+      process.exit(1); // Exemplo: sair do aplicativo em caso de falha na conexão
+    }
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+      console.log('Conexão com o banco de dados encerrada.');
+    });
   }
 }
